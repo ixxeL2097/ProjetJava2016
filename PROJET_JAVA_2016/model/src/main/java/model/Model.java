29096ff59cc01654b1468b312e4c364c9677c9a2
florian <florian@florian-PC>
2016-06-13 11:28:17 +0200

    Phase

--- PROJET_JAVA_2016/model/src/main/java/model/Model.java
+++ PROJET_JAVA_2016/model/src/main/java/model/Model.java
@@ -17,7 +17,7 @@ public class Model extends Observable implements IModel
 	/** The message. */
 	private String message;
 	private MapGenerator MapGenerator;
-	private String MapName = "C:/Users/FredPRO/git/ProjetJava2016/PROJET_JAVA_2016/MAP/MAP_lvl1.txt";
+	private String MapName = "MAP_lvl1.txt";
 
 	/**
 	 * Instantiates a new model.
