2a447ce3d1dffa020c0cae09d8a43a79f36593a9
florian <florian@florian-PC>
2016-06-19 15:36:37 +0200

    Ajout de tests unitaires JUnit -> Test de 4 getters -> Test de retour de boolean sur DBConnection

--- PROJET_JAVA_2016/model/src/main/java/model/DBConnection.java
+++ PROJET_JAVA_2016/model/src/main/java/model/DBConnection.java
@@ -9,9 +9,9 @@ final class DBConnection {
 	private static DBConnection	INSTANCE	= null;
 	private Connection					connection;
 
-	private DBConnection() 
+	public DBConnection() 
 	{
-		this.open();
+		this.ConnectToDB();
 	}
 	
 	public static synchronized DBConnection getInstance() {
@@ -22,10 +22,10 @@ final class DBConnection {
 	}
 
 
-	private Boolean open() {
+	public Boolean ConnectToDB() {
 		try {
 			Class.forName("com.mysql.jdbc.Driver");
-			this.connection = DriverManager.getConnection("jdbc:mysql://10.162.128.237:3307/jpu2016project?autoReconnect=true&useSSL=false","invite","motdepasse");
+			this.connection = DriverManager.getConnection("jdbc:mysql://localhost/jpu2016project?autoReconnect=true&useSSL=false","root","");
 		} catch (final ClassNotFoundException e) {
 			e.printStackTrace();
 		} catch (final SQLException e) {
---
+++ PROJET_JAVA_2016/model/src/main/java/model/DBConnectionTest.java
@@ -0,0 +1,33 @@
+package model;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class DBConnectionTest 
+{
+	private DBConnection DBC_init;
+
+	@Before
+	public void setUp() throws Exception 
+	{
+		this.DBC_init = new DBConnection();
+	}
+	@Test
+	public void testConnectToDB() 
+	{
+		if(this.DBC_init.ConnectToDB() == true)
+		{
+			System.out.println("ConnecToDB return true");
+		}
+		else if(this.DBC_init.ConnectToDB() == false)
+		{
+			System.out.println("ConnecToDB return false");
+		}
+		
+		// Check if DBConnectionJDB returns true. True => connection success
+		assertTrue(this.DBC_init.ConnectToDB());
+	}
+
+}
